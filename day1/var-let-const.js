8912bb4b586071e0c5781477ffb95ddb1efd46f3
Mehmet Gökhan <66775623+mgokhandev@users.noreply.github.com>
2021-09-22 16:01:24 +0300

    Hello and Varibles

---
+++ day1/var-let-const.js
@@ -0,0 +1,10 @@
+// 1. Let tipindeki değişkenin içeriğini daha sonradan değiştirebiliyoruz. 
+// let name = 'Gokhan';
+// name = 'Dogan';
+// console.log(name);
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+console.log("i'nin son degeri:", i);
