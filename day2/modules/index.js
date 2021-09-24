c758b286534d299bc63e80b1133e7f1f3404bf13
Mehmet Gökhan <66775623+mgokhandev@users.noreply.github.com>
2021-09-24 23:53:10 +0300

    Day2 - Part2

---
+++ day2/modules/index.js
@@ -0,0 +1,15 @@
+import { topla as t, cikar, carp, bol } from './myModule.js';
+
+console.log(t(1, 4));
+console.log(cikar(1, 4));
+console.log(carp(1, 4));
+console.log(bol(1, 4));
+
+// 2. Yöntem
+
+// import Mat from './myModule.js';
+
+// console.log(Mat.topla(1, 4));
+// console.log(Mat.cikar(1, 4));
+// console.log(Mat.carp(1, 4));
+// console.log(Mat.bol(1, 4));
---
+++ day2/modules/myModule.js
@@ -0,0 +1,13 @@
+export const topla = (a, b) => a + b;
+export const cikar = (a, b) => a - b;
+export const carp = (a, b) => a * b;
+export const bol = (a, b) => a / b;
+
+
+// 2. Yöntem
+// export default {
+//     topla: (a, b) => a + b,
+//     cikar: (a, b) => a - b,
+//     carp: (a, b) => a * b,
+//     bol: (a, b) => a / b,
+// }
